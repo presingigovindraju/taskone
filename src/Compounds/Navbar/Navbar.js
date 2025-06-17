dfd2843d134fd973548f0d3ee4de61d18ccb9af2
presingi_govind_raju <presingigovindraju333@gmail.com>
2025-06-17 17:21:50 +0530

    final

--- src/Compounds/Navbar/Navbar.js
+++ src/Compounds/Navbar/Navbar.js
@@ -2,11 +2,9 @@ import React from "react";
 import Drop from "./Drop/Drop.js";
 import "./Navbar.css";
 
-
-
-const Navbar =({setHide})=>{
-    return (
-        <nav className="navBar">
+const Navbar = ({ setHide }) => {
+  return (
+    <nav className="navBar">
       <div id="navContentDiv">
         <div id="firstOne">
           {/* <div id="displayMedia" >
@@ -26,12 +24,13 @@ const Navbar =({setHide})=>{
               src="https://shop.drivemedical.com/_ui/responsive/common/images/flag.png"
               alt="flag"
             />
-            <h4 id="usen" className="gapBtw">US EN</h4>
+            <h4 id="usen" className="gapBtw">
+              US EN
+            </h4>
             <i className="hamani gapBtw fa-solid fa-angle-down"></i>
           </div>
 
-
-          <div id="firstOneSecoundDiv" >
+          <div id="firstOneSecoundDiv">
             <a
               href="https://www.google.com/maps/@30.4380174,-84.3038144,17z?entry=ttu&g_ep=EgoyMDI0MTEyNC4xIKXMDSoASAFQAw%3D%3D"
               target="_blank"
@@ -41,7 +40,6 @@ const Navbar =({setHide})=>{
             </a>
           </div>
 
-
           <div id="firstOneThirdDiv">
             <a href="https://chatgpt.com/" target="_blank">
               <i className="gapBtw fa-solid fa-comment-dots"></i>
@@ -49,14 +47,13 @@ const Navbar =({setHide})=>{
             </a>
           </div>
 
-
-          <p id="inspiredOne" className="gapBtw">Inspired By Drive</p>
-
+          <p id="inspiredOne" className="gapBtw">
+            Inspired By Drive
+          </p>
         </div>
 
-
-        <div id="secoundOne" >
-        <i id="menuDrop" class="fa-solid fa-bars"></i>
+        <div id="secoundOne">
+          <i id="menuDrop" class="fa-solid fa-bars"></i>
           <div id="secoundOneFirstDiv">
             <i className=" fa-solid fa-box-archive"></i>
             <p className="justOrder ">Order Status</p>
@@ -71,9 +68,8 @@ const Navbar =({setHide})=>{
           </div>
         </div>
       </div>
-        </nav>
-    )
-}
+    </nav>
+  );
+};
 
-export default Navbar
-    
+export default Navbar;
